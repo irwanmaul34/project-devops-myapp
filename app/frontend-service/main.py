a4e19001320f43d0dba4ab4dd79f08b02b5056fc
Irwan Maulana <irwanmaul34@gmail.com>
2026-04-14 23:19:39 +0000

    revisi python

--- app/frontend-service/main.py
+++ app/frontend-service/main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+# folder templates wajib ada di /app/templates
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request
+        }
     )
